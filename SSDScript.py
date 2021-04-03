f202cd65b59c67f187a0b48c9fa3553aa65c6012
narendra974 <narendra.974@gmail.com>
2021-04-04 03:24:24 +0530

    update SSDScript.py
    update SSDScript.py

--- SSDScript.py
+++ SSDScript.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import MaxPool2D, Conv2D, Reshape, Concatenate, Activation
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.applications import VGG16
-import custom_layers.L2Normalization as L2Normalization
-import custom_layers.DefaultBoxes as DefaultBoxes
-import custom_layers.DecodeSSDPredictions as DecodeSSDPredictions
+from l2_normalization import L2Normalization
+from default_boxes import DefaultBoxes
+from decode_ssd_predictions import DecodeSSDPredictions
 from utils.ssd_utils import get_number_default_boxes

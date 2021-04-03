374140b8a3aad5cd578450a673157435591a6e25
narendra974 <narendra.974@gmail.com>
2021-04-04 01:57:41 +0530

    Changing L2Normalisation to l2Normalisation.
    Changing L2Normalisation to l2Normalisation.

--- SSDScript.py
+++ SSDScript.py
@@ -3,5 +3,5 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import MaxPool2D, Conv2D, Reshape, Concatenate, Activation
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.applications import VGG16
-from custom_layers import L2Normalization, DefaultBoxes, DecodeSSDPredictions
+from custom_layers import l2Normalization, DefaultBoxes, DecodeSSDPredictions
 from utils.ssd_utils import get_number_default_boxes
